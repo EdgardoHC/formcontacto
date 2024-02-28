e46d8c001e1eaa78abcc4c6d48922bfdc3ce4873
jhenriquez <jhenriquez@STINFOR017EE>
2024-02-28 13:54:38 -0600

    Agregar archivos de proyecto.

---
+++ formcontacto/Pages/contacto.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace formcontacto.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            // Lógica para manejar el envío del formulario aquí
+        }
+    }
+}
